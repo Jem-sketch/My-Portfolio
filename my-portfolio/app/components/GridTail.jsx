3b0225aaa3193414150aa559ae6ad8ee026e303f
Jem Celestial <jemCelestialj1@gmail.com>
2026-08-11 04:39:51 +0800

    Creating new Grid tail and removing Cursor Ring

---
+++ my-portfolio/app/components/GridTail.jsx
@@ -0,0 +1,19 @@
+
+"use client";
+
+import { useEffect, useRef } from "react"; 
+import style from "./GridEffect.module.css";
+
+export default function GridEffect(){
+
+    const CELL_SIZE = 40;
+    const COLOR_R = 79;
+    const COLOR_G = 38;
+    const COLOR_B = 233;
+    const STARTING_ALPHA = 255;
+    const BACKGROUND_COLOR = 31;
+    const PROB_OF_NEIGHBOR = 0.5;
+    const AMT_FADE_PER_FRAME = 5;
+    const STROKE_WEIGHT = 1;
+
+}
--- my-portfolio/app/layout.js
+++ my-portfolio/app/layout.js
@@ -30,7 +30,6 @@ export default function RootLayout({ children }) {
       
       <body>
         <IntroVideo />
-        <CursorRing />
         <Navbar />
 
         <main>
